8a97b16e71ac67d91ce739eae6f47440544343a0
Sean Park <sean@example.com>
2026-02-25 21:38:24 +0100

    feat: add tailwind css support and configuration

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+        "./index.html",
+        "./main.jsx",
+        "./prelude.jsx",
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
